9812af2d1508113afbe6aa02b48b4ca9d9f564a7
Saule BN <beyonce@thinkful.com>
2017-06-19 17:09:58 -0400

    initial files

---
+++ W6D1/hot_cold/src/components/hotCold_Board.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './hotCold_Board.css';
+
+export default function hotCold_Board() {
+    return (
+        <div className="hotColdboard">
+            <h2>Hot or Cold</h2>
+            <div className="links">
+               <h3>New Game</h3>
+            </div>
+        </div>
+    );
+}
--- W6D1/hot_cold/src/index.js
+++ W6D1/hot_cold/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import hotColdApp from './hotCold';
+import hotColdApp from './components/hotCold_Board';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<hotColdApp />, document.getElementById('root'));
+ReactDOM.render(<hotCold_Board />, document.getElementById('root'));
 registerServiceWorker();
